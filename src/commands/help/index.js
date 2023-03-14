d562071691ec4df2693d5bbac31d1587ba0ad290
RedFeaTherXll <yuchi9716@gmail.com>
2023-03-14 19:49:19 +0800

    幫助系統表情符號更新

--- src/commands/help/index.js
+++ src/commands/help/index.js
@@ -58,10 +58,10 @@ export const action = async (interaction) =>{
     const Help = new EmbedBuilder()
     .setColor("#939787")
     .setTitle(`${client.user.username}`)
-    .setDescription("歡迎使用，你可以查看下面的所有指令 <:Snows:1084361500136845372>")
+    .setDescription("歡迎使用，你可以查看下面的所有指令 <:help:1085139950485393468>")
     .addFields(
       { name: '使用下面的選單選出您想要尋求幫助的指令吧', value: ' ' },
-      { name: '`/` 斜線指令已啟用 <a:pickcheckmark:1084383521155592212>', value: '使用 / 作為前綴來使用指令', inline: true },
+      { name: '`/` 斜線指令已啟用 <a:pickcheckmark:1084383521155592212>', value: '使用 / 作為前綴來使用指令 <:dev:1085136540918882324>', inline: true },
     )
     .setTimestamp()
     .setFooter({ text: '@2023 KOSHKA-LENGEND', iconURL: 'https://i.imgur.com/clEn73Q.gif' });

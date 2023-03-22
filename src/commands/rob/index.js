4af1add56a5aaa85fa695886715cf119356b0a32
RedFeaTherXll <yuchi9716@gmail.com>
2023-03-22 22:34:49 +0800

    嘗試修復搶劫未回應問題

--- src/commands/rob/index.js
+++ src/commands/rob/index.js
@@ -20,16 +20,15 @@ export const action = async (interaction) =>{
     const appStore = useAppStore()
     const client = appStore.client;
     if(timeout.includes(interaction.user.id)) return await interaction.reply({embeds: [timeE],ephemeral:true})
-    
     const { options, user, guild} = interaction;
-
+    
 
     const userStealing = options.getUser('使用者');
     let Data = await ecoSchema.findOne({Guild: guild.id, User: user.id})
     let DataUser = await ecoSchema.findOne({Guild: guild.id, User: userStealing.id})
 
     if(!Data) return await interaction.reply({content: `<a:wrong:1085174299628929034>丨你無法進行搶劫 <a:bunbun:991105824170713088>\n因為你沒有帳戶`, ephemeral: true});
-    if(userStealing == interaction.user) return interaction.reply({content: `<a:wrong:1085174299628929034>丨你無法搶劫你自己 <a:bunbun:991105824170713088>`, ephemeral: true});
+    if(userStealing == interaction.user) return await interaction.reply({content: `<a:wrong:1085174299628929034>丨你無法搶劫你自己 <a:bunbun:991105824170713088>`, ephemeral: true});
     if(!DataUser) return await interaction.reply({content: `<a:wrong:1085174299628929034>丨你無法搶劫他\n因為該使用者沒有帳戶 <a:bunbun:991105824170713088>`, ephemeral: true});
     if(DataUser.Wallet <= 0) return await interaction.reply({content: `<a:wrong:1085174299628929034>丨你無法搶劫他 <a:bunbun:991105824170713088>\n**他身上沒有錢QQ**`, ephemeral: true});
 
@@ -71,27 +70,28 @@ export const action = async (interaction) =>{
       await DataUser.save();
 
     } else if(value < 0) {
-      if(isNaN(value)) return await interaction.reply({content: `> <a:bunbun:991105824170713088> 他發現你要搶劫他了\n> 所以他打給了警察\n> 但你很幸運地逃走了。沒有任何損失!`, ephemeral: true})
-      
-      let nonSymbol;
-      if(value - Data.Wallet < 0) {
+        if(isNaN(value)) return await interaction.reply({content: `> <a:bunbun:991105824170713088> 他發現你要搶劫他了\n> 所以他打給了警察\n> 但你很幸運地逃走了。沒有任何損失!`, ephemeral: true})
+        
+        let nonSymbol;
+        if(value - Data.Wallet < 0) {
 
-        const stringV = `${value}`
-        nonSymbol = await stringV.slice(1);
+          const stringV = `${value}`
+          nonSymbol = await stringV.slice(1);
 
+        const beblostEmbed = new EmbedBuilder()
+        .setColor('Red')
+        .setTitle('<a:wrong:1085174299628929034> 搶劫失敗! <a:bunbun:991105824170713088> ')
+        .addFields({name: `${user.tag} **搶劫了** ${userStealing.tag}`,value: `> 但你失敗了!`});
+
+        return await interaction.reply({embeds: [beblostEmbed]})
+      }
       const beblostEmbed = new EmbedBuilder()
-      .setColor('Red')
-      .setTitle('<a:wrong:1085174299628929034> 搶劫失敗! <a:bunbun:991105824170713088> ')
-      .addFields({name: `${user.tag} **搶劫了** ${userStealing.tag}`,value: `> 但你失敗了!`});
+        .setColor('Red')
+        .setTitle('<a:wrong:1085174299628929034> 搶劫失敗! <a:bunbun:991105824170713088> ')
+        .addFields({name: `${user.tag} **搶劫了** ${userStealing.tag}`,value: `> 但你失敗了!`});
 
-      await interaction.reply({embeds: [beblostEmbed]})
+        return await interaction.reply({embeds: [beblostEmbed]})
     }
-
-    timeout.push(interaction.user.id);
-    setTimeout(()=>{
-      timeout.shift();
-    }, 30000)
-    } 
   } catch (error) {
     console.log(`/搶劫 有錯誤 ${error}`);
     const errorCode = new EmbedBuilder()
@@ -102,5 +102,8 @@ export const action = async (interaction) =>{
     .setTimestamp()  
     return await interaction.reply({embeds: [errorCode]})
   }
-  
+  timeout.push(interaction.user.id);
+    setTimeout(()=>{
+      timeout.shift();
+    }, 3000);
 }

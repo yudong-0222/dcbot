82644954ab57fae4917bccbb2ebf94d3a1f4cb84
RedFeaTherXll <yuchi9716@gmail.com>
2023-03-27 19:48:51 +0800

    打工指令-老師指令完工

--- src/commands/work/index.js
+++ src/commands/work/index.js
@@ -2,6 +2,9 @@ import {SlashCommandBuilder, EmbedBuilder, ActionRowBuilder, ButtonBuilder, Butt
 import {useAppStore} from '../../store/app'
 import ecoSchema from '../../Schemas/ecoSchema'
 import { StringSelectMenuBuilder } from '@discordjs/builders'
+import workSchema from '../../Schemas/workSchema'
+
+const cooldowns = new Map();
 
 export const command = new SlashCommandBuilder()
 .setName('job')
@@ -16,15 +19,31 @@ export const command = new SlashCommandBuilder()
   .setName('取消工作')
   .setDescription('取消工作,你將領不到任何工資!')
   )
+.addSubcommand(cmd=>
+  cmd
+  .setName('開課')
+  .setDescription('老師的工作!')
+  )
 
 export const action = async (interaction) =>{
   try {
-    
     const appStore = useAppStore()
     const client = appStore.client;
     const {user} = interaction
+    const now = Date.now();
+    const cooldownSeconds = 300;
+
     let Data = await ecoSchema.findOne({Guild: interaction.guild.id, User: user.id});
     if(!Data) return await interaction.reply({content: `<a:wrong:1085174299628929034>丨你無法進行打工 <:jobs:1088446692262674492> \n因為你沒有帳戶`, ephemeral: true});
+    let workla = await workSchema.findOne({Guild: interaction.guild.id, User: user.id})
+    if (!workla) {
+      workla = new workSchema ({
+        Guild: interaction.guild.id,
+        User: user.id,
+        Work: "",
+      })
+      await workla.save();
+    }
     if(Data.isWorking === null) {
       const oldAccount = new EmbedBuilder()
       .setColor('Red')
@@ -56,7 +75,6 @@ export const action = async (interaction) =>{
         description: "工地乃一處僅鋼筋佇立之處,乃粗重,乃悶熱.高俸祿而勞",
       },
     ];
-
     
     const firstMsg = new EmbedBuilder()
       .setColor('Random')
@@ -81,7 +99,7 @@ export const action = async (interaction) =>{
     const command = interaction.options.getSubcommand();
     switch(command) {
       case '找工作':
-          if(Data.isWorking) return interaction.reply({components: [], embeds: [], content: `<a:wrong:1085174299628929034>丨你無法尋找工作! <:jobs:1088446692262674492> \n因為你已經有工作了`, ephemeral: true})
+          if(Data.isWorking) return interaction.reply({components: [], embeds: [], content: `<a:wrong:1085174299628929034>丨你無法尋找工作! <:jobs:1088446692262674492> \n因為你已經有工作了\n> 你的工作是: \`${workla.Work}\``, ephemeral: true})
           const selectionRespond = await interaction.reply({embeds: [firstMsg], components: [jobSelect]})
           const collector = await selectionRespond.createMessageComponentCollector({ ComponentType: ComponentType.StringSelect, ComponentType: ComponentType.Button})
           collector.on("collect", async (i)=>{
@@ -115,9 +133,11 @@ export const action = async (interaction) =>{
                       i.reply({embeds: [teacher], ephemeral: true, components: [btn]})
                       collector.on("collect", async (b)=> {
                         if(b.customId === "tea" && b.member.id === user.id) {
-                          if(Data.isWorking) return i.editReply({components: [], embeds: [], content: `<a:wrong:1085174299628929034>丨你無法做此項工作!\n因為你已經有工作了 <:jobs:1088446692262674492>`, ephemeral: true})
+                          if(Data.isWorking) return i.editReply({components: [], embeds: [], content: `<a:wrong:1085174299628929034>丨你無法尋找工作! <:jobs:1088446692262674492> \n因為你已經有工作了\n> 你的工作是: \`${workla.Work}\``, ephemeral: true})
                           Data.isWorking = true;
                           await Data.save();
+                          workla.Work = "老師";
+                          await workla.save();
                           i.editReply({components: [], embeds: [lastMsg]})
                         }
                       })
@@ -148,9 +168,11 @@ export const action = async (interaction) =>{
                       i.reply({embeds: [teacher], ephemeral: true, components: [btn]})
                       collector.on("collect", async (b)=> {
                         if(b.customId === "fisher" && b.member.id === user.id) {
-                          if(Data.isWorking) return i.editReply({components: [], embeds: [], content: `<a:wrong:1085174299628929034>丨你無法做此項工作!\n因為你已經有工作了 <:jobs:1088446692262674492>`, ephemeral: true})
+                          if(Data.isWorking) return i.editReply({components: [], embeds: [], content: `<a:wrong:1085174299628929034>丨你無法尋找工作! <:jobs:1088446692262674492> \n因為你已經有工作了\n> 你的工作是: \`${workla.Work}\``, ephemeral: true})
                           Data.isWorking = true;
                           await Data.save();
+                          workla.Work = "漁夫";
+                          await workla.save();
                           i.editReply({components: [], embeds: [lastMsg]})
                         }
                       })
@@ -182,9 +204,11 @@ export const action = async (interaction) =>{
                       i.reply({embeds: [teacher], ephemeral: true, components: [btn]})
                       collector.on("collect", async (b)=> {
                         if(b.customId === "watcher" && b.member.id === user.id) {
-                          if(Data.isWorking) return i.editReply({components: [], embeds: [], content: `<a:wrong:1085174299628929034>丨你無法做此項工作!\n因為你已經有工作了 <:jobs:1088446692262674492>`, ephemeral: true})
+                          if(Data.isWorking) return i.editReply({components: [], embeds: [], content: `<a:wrong:1085174299628929034>丨你無法尋找工作! <:jobs:1088446692262674492> \n因為你已經有工作了\n> 你的工作是: \`${workla.Work}\``, ephemeral: true})
                           Data.isWorking = true;
                           await Data.save();
+                          workla.Work = "外籍看護";
+                          await workla.save();                      
                           i.editReply({components: [], embeds: [lastMsg]})
                         }
                       })
@@ -215,9 +239,11 @@ export const action = async (interaction) =>{
                       i.reply({embeds: [teacher], ephemeral: true, components: [btn]})
                       collector.on("collect", async (b)=> {
                         if(b.customId === "workers" && b.member.id === user.id) {
-                          if(Data.isWorking) return i.editReply({components: [], embeds: [], content: `<a:wrong:1085174299628929034>丨你無法做此項工作!\n因為你已經有工作了 <:jobs:1088446692262674492>`, ephemeral: true})
+                          if(Data.isWorking) return i.editReply({components: [], embeds: [], content: `<a:wrong:1085174299628929034>丨你無法尋找工作! <:jobs:1088446692262674492> \n因為你已經有工作了\n> 你的工作是: \`${workla.Work}\``, ephemeral: true})
                           Data.isWorking = true;
                           await Data.save();
+                          workla.Work = "工地人";
+                          await workla.save();                  
                           i.editReply({components: [], embeds: [lastMsg]})
                         }
                       })
@@ -231,61 +257,123 @@ export const action = async (interaction) =>{
           })
       }
 
-      switch(command) {
-        case '取消工作':
-          if(Data.isWorking === false){
-            return await interaction.reply({content: `<a:wrong:1085174299628929034>丨你目前沒有工作! <:jobs:1088446692262674492> \n使用 \`/打工 找工作\` 尋找一個工作吧!`, ephemeral: true});
+      /*取消工作*/
+    switch(command) {
+      case '取消工作':
+        if(Data.isWorking === false){
+          return await interaction.reply({content: `<a:wrong:1085174299628929034>丨你目前沒有工作! <:jobs:1088446692262674492> \n使用 \`/打工 找工作\` 尋找一個工作吧!`, ephemeral: true});
+        }
+        const cacelJob = new EmbedBuilder()
+        .setColor('Red')
+        .setTitle('<:warn:1085138987636752414>丨確定要取消工作嗎')
+        .setDescription("如果你取消工作,那麼你將無法拿到本次的工資\n**不管做多久,沒做完都一樣拿不到**\n\`\`\`\請確認是否要取消?`\`\`") 
+        .setTimestamp()  
+
+        const btn = new ActionRowBuilder()
+        .addComponents(
+          new ButtonBuilder()
+          .setCustomId('yes')
+          .setLabel('是的!我要取消')
+          .setEmoji(`<a:checked:1086296113818128414>`)
+          .setStyle(ButtonStyle.Success)
+        )
+
+        const btn2 = new ActionRowBuilder()
+        .addComponents(
+          new ButtonBuilder()
+          .setCustomId('no')
+          .setLabel('我沒有要取消')
+          .setEmoji(`<a:Animatederror:1086903258993406003>`)
+          .setStyle(ButtonStyle.Danger)
+        )
+
+
+        const cancelMsg = await interaction.reply({embeds: [cacelJob], components:[btn,btn2], ephemeral: true})
+        const collect = cancelMsg.createMessageComponentCollector({ComponentType: ComponentType.Button})
+        collect.on("collect", async(i)=> {
+          if(i.customId === 'yes' && i.member.id === user.id) {
+            const yesIdo = new EmbedBuilder()
+            .setColor('Green')
+            .setTitle('<a:pinkcheckmark:1084383521155592212>丨你結束了你的工作')
+            .setDescription(`因為提早結束,所以你沒有拿到任何工資 <a:moneyanimated:1089137556496584805>`)
+            .setTimestamp()  
+            interaction.editReply({embeds: [yesIdo], components: [], ephemeral: true})
+            Data.isWorking = false;
+            await Data.save();
+            workla.Work = "";
+            await workla.save();
           }
-          const cacelJob = new EmbedBuilder()
+          if (i.customId === 'no' && i.member.id === user.id) {
+            const noIdont = new EmbedBuilder()
+            .setColor('Red')
+            .setTitle('<a:checkpurple:1089136864168001556>丨你保留了你的工作')
+            .setTimestamp()  
+            return interaction.editReply({embeds: [noIdont], components: [], ephemeral: true})
+          }
+        })
+    }
+
+    switch(command) {
+      case "開課":
+        if (cooldowns.has(user)) {
+          const cooldownEnd = cooldowns.get(user) + cooldownSeconds * 1000;
+          const secondLeft = Math.round((cooldownEnd - now) / 1000 );
+          
+            if (0 < cooldownEnd) {
+                  const min = Math.floor(secondLeft / 60);
+                  const embed = new EmbedBuilder()
+                  .setColor('Red')
+                  .setTitle(`<a:Animatederror:1086903258993406003>|工作尚未結束!`)
+                  .setDescription(`你還需要等\`${min}\`分 \`${secondLeft}\` 秒\n才能再次使用!`)
+
+                  return await interaction.reply({embeds: [embed]})
+            }
+          }
+        }
+        const cooldownExpiration = Date.now() + (cooldownSeconds * 1000);
+        cooldowns.set(user, cooldownExpiration);
+        
+
+        await interaction.deferReply({ephemeral: false});
+        if (!Data.isWorking) {
+          const noJobs = new EmbedBuilder()
           .setColor('Red')
-          .setTitle('<:warn:1085138987636752414>丨確定要取消工作嗎')
-          .setDescription("如果你取消工作,那麼你將無法拿到本次的工資\n**不管做多久,沒做完都一樣拿不到**\n\`\`\`\請確認是否要取消?`\`\`") 
-          .setTimestamp()  
-  
-          const btn = new ActionRowBuilder()
-          .addComponents(
-            new ButtonBuilder()
-            .setCustomId('yes')
-            .setLabel('是的!我要取消')
-            .setEmoji(`<a:checked:1086296113818128414>`)
-            .setStyle(ButtonStyle.Success)
-          )
-  
-          const btn2 = new ActionRowBuilder()
-          .addComponents(
-            new ButtonBuilder()
-            .setCustomId('no')
-            .setLabel('我沒有要取消')
-            .setEmoji(`<a:Animatederror:1086903258993406003>`)
-            .setStyle(ButtonStyle.Danger)
-          )
-  
-  
-          const cancelMsg = await interaction.reply({embeds: [cacelJob], components:[btn,btn2], ephemeral: true})
-          const collect = cancelMsg.createMessageComponentCollector({ComponentType: ComponentType.Button})
-          collect.on("collect", async(i)=> {
-            if(i.customId === 'yes' && i.member.id === user.id) {
-              const yesIdo = new EmbedBuilder()
-              .setColor('Green')
-              .setTitle('<a:pinkcheckmark:1084383521155592212>丨你結束了你的工作')
-              .setDescription(`因為提早結束,所以你沒有拿到任何工資 <a:moneyanimated:1089137556496584805>`)
-              .setTimestamp()  
-              interaction.editReply({embeds: [yesIdo], components: [], ephemeral: true})
-              Data.isWorking = false;
-              await Data.save();
-            }
-            if (i.customId === 'no' && i.member.id === user.id) {
-              const noIdont = new EmbedBuilder()
-              .setColor('Red')
-              .setTitle('<a:checkpurple:1089136864168001556>丨你保留了你的工作')
-              .setTimestamp()  
-              return interaction.editReply({embeds: [noIdont], components: [], ephemeral: true})
-            }
-          })
-      }
+          .setTitle('<a:Animatederror:1086903258993406003>丨你無法使用!')
+          .setDescription("原因:\n因為你當前沒有工作\n`/打工` 來尋找一份打工") 
+          .setTimestamp()
+          return await interaction.editReply({embeds: [noJobs]});
+        }
+        if (!(workla.Work === "老師")) {
+          const notThisJob = new EmbedBuilder()
+          .setColor('Red')
+          .setTitle('<a:Animatederror:1086903258993406003>丨你無法使用!')
+          .setDescription("原因:\n因為你不是 **老師**\n`/打工` 來尋找一份打工") 
+          .setTimestamp()
+
+          return interaction.editReply({embeds: [notThisJob]})
+        } 
+        let pay = Math.round(Math.random() * 33 ) + 120;
+        if (pay >= 150) pay = 150;
+        const doThings = [
+          "> 使用 **摸魚**: 這節課自修\n預估 \`5分鐘\` 後得到 <a:coin1:1087317662998208602>",
+          "> **認真教學**,開課獲得學生青睞\n預估\`5分鐘\` 後得到 <a:coin1:1087317662998208602>",
+          "> **履行《師說》的內容**,傳其道解其惑也\n預估\`5分鐘\` 後得到 <a:coin1:1087317662998208602>",
+          "> <:p_:867801170055921674> **肥龍式幹話**的教學方式\n預估\`5分鐘\` 後得到 <a:coin1:1087317662998208602>",
+          "> <a:eggcat:1084364519599521792> **考試**,這堂課你超爽\n預估\`5分鐘\` 後得到 <a:coin1:1087317662998208602>",
+        ];
+        const doThingN = Math.floor(Math.random() * doThings.length);
+        const lastMessage = new EmbedBuilder()
+        .setColor('Green')
+        .setTitle(`👨‍🏫 | 名師開課 <a:green_tick:994529015652163614>`)
+        .setDescription(`${doThings[[doThingN]]} $${pay}`);
+        
+        setTimeout(async() => {
+          Data.Bank += pay;
+          await Data.save();
+        }, 5 * 60 * 1000)
+
+        await interaction.editReply({embeds: [lastMessage], components: []});
       
-    
-    
   } catch (error) {
     console.log(`/打工 有錯誤: ${error}`);
     const errorCode = new EmbedBuilder()

6ccf10ae3042275bef18b57df1cd076fa1480def
RedFeaTherXll <yuchi9716@gmail.com>
2023-04-03 18:28:47 +0800

    v 0.356

--- src/commands/botinfo/index.js
+++ src/commands/botinfo/index.js
@@ -9,7 +9,7 @@ export const action = async (interaction) =>{
   try {
     const appStore = useAppStore()
     const client = appStore.client;
-    const Ver = "`V0.354`";
+    const Ver = "`V0.356`";
     const rank = "`專家II`";
     const author = `<@${interaction.guild.ownerId}>`;
     // const icon = `${client.user.displayAvatarURL()}`

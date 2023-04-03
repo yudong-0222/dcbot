3606213c8f867086a34e894266f23393b4a5a71b
RedFeaTherXll <yuchi9716@gmail.com>
2023-04-03 13:56:55 +0800

    新增內文判斷--機率

--- src/main.js
+++ src/main.js
@@ -10,10 +10,25 @@ dotenv.config()
 
 loadCommands()
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({ 
+  intents: [
+    GatewayIntentBits.DirectMessages,
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ] });
 const appStore = useAppStore()
 appStore.client = client
-
 loadEvents()
+
+client.on("messageCreate", (message) => {
+    if (message.content.includes("機率")) {
+      var qq = ["12%","54%","9%","87%","43%"," 0%","34%","77%","98%","69%","27%","11%","71%","3%","6%","51%","47%","3%",]
+      let contne = qq[Math.floor(Math.random()* qq.length)]
+      message.channel.send(`${contne}`)
+    }
+})
+
+
 
 client.login(process.env.TOKEN);
